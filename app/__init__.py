e6d2c816b1e757a08d81e2bd726db991133d1d70
Vadim Spac <shvadim92232@gmail.com>
2026-03-11 14:20:51 +0100

    correction init

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from app.extensions import db, login_manager
 from app.config import Config
-
+import os
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        template_folder=os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),
+            "templates"
+        ),
+    )
     app.config.from_object(config_class)
 
     db.init_app(app)
